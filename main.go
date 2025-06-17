4bb0331264526e72c5abaa754d17e05fa0b43345
Fisherman August <fshrmn.august@gmail.com>
2025-06-17 21:45:00 +0300

    Como estas?

--- main.go
+++ main.go
@@ -2,15 +2,12 @@ package main
 
 import (
 	"fmt"
-	"unicode/utf8"
+	_ "log"
 )
 
 func main() {
 	question := "¿Cómo estás?"
-
-	fmt.Println(len(question), "bytes")
-	fmt.Println(utf8.RuneCountInString(question), "runes")
-
-	c, size := utf8.DecodeRuneInString(question)
-	fmt.Printf("First rune: %c %v bytes", c, size)
+	for _, c := range question {
+		fmt.Printf("%c\n", c)
+	}
 }
