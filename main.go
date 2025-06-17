30f5a04d51a5ee89ca2728646e8f673d94fb84a5
Fisherman August <fshrmn.august@gmail.com>
2025-06-17 19:16:00 +0300

    all ascii symbols

--- main.go
+++ main.go
@@ -3,14 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	// strLiteral := "World"
-	// var strLiteral2 = "World"
-	// var strLiteral3 string = "World"
-
-	// var blankLiteral string // ""
-
-	var strLiteral = `Hello \n
-World \t !` //\n - new line, \t - tab
-	var path = "C:\\Users\\User\\asd"
-	fmt.Println(strLiteral)
+	for i := 0; i < 255; i++ {
+		fmt.Printf("%d - %v \n", i, string(byte(i)))
+	}
 }
