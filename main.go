5b110118afa1f0fca33bf89c881b63e33812e40c
Fisherman August <fshrmn.august@gmail.com>
2025-06-17 19:08:45 +0300

    string literals

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	// strLiteral := "World"
+	// var strLiteral2 = "World"
+	// var strLiteral3 string = "World"
+
+	// var blankLiteral string // ""
+
+	var strLiteral = `Hello \n
+World \t !` //\n - new line, \t - tab
+	var path = "C:\\Users\\User\\asd"
+	fmt.Println(strLiteral)
 }
